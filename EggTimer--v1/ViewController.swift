d207282cb7015a9d278ecc9425c47edff60e2635
aryanjagarwal <68804795+aryanjagarwal@users.noreply.github.com>
2022-06-10 10:05:29 +0530

    dictionary adding

--- EggTimer--v1/ViewController.swift
+++ EggTimer--v1/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let eggTimes = ["Soft": 5, "Medium": 8, "Hard": 10]
+//    let softTime = 5
+//    let mediumTime = 8
+//    let hardTime = 10
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        //print(sender.currentTitle)
+        let hardness = sender.currentTitle
+        
+//        if hardness == "Soft" {
+//            print(softTime)
+//        } else if hardness == "Medium" {
+//            print(mediumTime)
+//        } else {
+//            print(hardTime)
+//        }
+        
+//        switch hardness {
+//        case "Soft":
+//            print(softTime)
+//        case "Medium":
+//            print(mediumTime)
+//        case "Hard":
+//            print(hardTime)
+//        default:
+//            print("Error")
+//        }
     }
-
-
+    
 }
-
